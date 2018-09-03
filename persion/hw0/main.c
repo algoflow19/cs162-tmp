7c6c9b9f728906a06f083a4b2ff56dfe701df6b1
developerkk <2957691307@qq.com>
2018-09-03 19:53:01 +0800

    Done the job

--- persion/hw0/main.c
+++ persion/hw0/main.c
@@ -5,10 +5,10 @@
 int main() {
     struct rlimit lim;
     getrlimit(RLIMIT_STACK,&lim);
-    printf("stack size: %lu\n", lim.rlim_max);
+    printf("stack size: %lu\n", lim.rlim_cur);
     getrlimit(RLIMIT_NPROC,&lim);
-    printf("process limit: %ld\n", lim.rlim_max);
+    printf("process limit: %ld\n", lim.rlim_cur);
     getrlimit(RLIMIT_NOFILE,&lim);
-    printf("max file descriptors: %ld\n", lim.rlim_max);
+    printf("max file descriptors: %ld\n", lim.rlim_cur);
     return 0;
 }
