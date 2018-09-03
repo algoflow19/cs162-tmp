59bdf5f3e06a4ab3600dc951189f551d3743a650
developerkk <2957691307@qq.com>
2018-09-03 18:57:21 +0800

    did map

--- persion/hw0/map.c
+++ persion/hw0/map.c
@@ -10,7 +10,7 @@ int recur(int i) {
     int j = i;
 
     /* TODO 1: Fix this so it prints the address of j */
-    printf("recur call %d: stack@ %p\n", i, j);
+    printf("recur call %d: stack@ %p\n", i, &j);
 
     if (i > 0) {
         return recur(i - 1);
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
     int i;
 
     /* Dynamically allocate some stuff */
-    char *buf1 = malloc(100);
+    char *buf1 = malloc(110);
     /* ... and some more stuff */
     char *buf2 = malloc(100);
 
@@ -35,13 +35,14 @@ int main(int argc, char *argv[]) {
     printf("recur @ %p\n", recur);
 
     /* TODO 2: Fix to get address of the stack variable */
-    printf("_main stack: %p\n", i);
+    printf("_main stack: %p\n", (void*)&i);
 
     /* TODO 3: Fix to get address of a static variable */
-    printf("static data: %p\n", stuff);
+    printf("static data: %p\n", (void*)&stuff);
 
     printf("Heap: malloc 1: %p\n", buf1);
     printf("Heap: malloc 2: %p\n", buf2);
     recur(3);
     return 0;
 }
+
